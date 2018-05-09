0089b068394d64ef79fb1667ca2e9407601c2287
Seif Eddine Ghezala <siffogh3@gmail.com>
2018-05-09 19:40:18 +0200

    initial react component
    ss

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import WeightDonut from "../lib/WeightDonut";
+
+const App = () => <WeightDonut />;
+
+ReactDOM.render(<App />, document.getElementById("root"));
